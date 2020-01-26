58ac98517e47302436c3e9053aff5d35b2733dc6
Максим Медяник <tvoygluk@gmail.com>
2020-01-27 00:03:34 +0300

    Добавляет окно подтверждения покупки и интерактив в js

--- build/js/script.js
+++ build/js/script.js
@@ -1,17 +1,33 @@
-'use strict';
-// var pageHeader = document.querySelector('.page-header');
-// var headerToggle = document.querySelector('.page-header__toggle');
+let zodiac = document.querySelectorAll('.select input');
+let photoName = document.querySelector('.pendant__about h3');
+let orderName = document.querySelector('.order__sign p');
+let backImg = document.querySelector('.pendant img:first-of-type');
+let frontImg = document.querySelector('.pendant img:last-of-type');
 
-// pageHeader.classList.remove('page-header--nojs');
+let zodiacList = {
+  aries: 'Овен',
+  taurus: 'Телец',
+  gemini: 'Близнецы',
+  cancer: 'Рак',
+  leo: 'Лев',
+  virgo: 'Дева',
+  libra: 'Весы',
+  scorpio: 'Скорпион',
+  sagittarius: 'Стрелец',
+  capricorn: 'Козерог',
+  aquarius: 'Водолей',
+  pisces: 'Рыбы'
+};
 
-// headerToggle.addEventListener('click', function () {
-//   if (pageHeader.classList.contains('page-header--closed')) {
-//     pageHeader.classList.remove('page-header--closed');
-//     pageHeader.classList.add('page-header--opened');
-//   } else {
-//     pageHeader.classList.add('page-header--closed');
-//     pageHeader.classList.remove('page-header--opened');
-//   }
-// });
-
-// document.querySelector('footer').style.backgroundColor = 'red';
+zodiac.forEach(function (item) {
+    item.addEventListener('change', function () {
+        photoName.textContent = zodiacList[item.value];
+        orderName.textContent = zodiacList[item.value];
+        backImg.src = `img/zodiac-${item.value}.png`;
+        backImg.srcset = `img/zodiac-${item.value}@2x.png`;
+        frontImg.src = `img/${item.value}.png`;
+        frontImg.srcset = `img/${item.value}@2x.png`;
+      }
+    );
+  }
+);
